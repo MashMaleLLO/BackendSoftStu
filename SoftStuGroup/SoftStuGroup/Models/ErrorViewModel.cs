502da3b02e1ba7e6480ca7514b4b0f6302adf547
Narawich Youbua <narawich49@gmail.com>
2022-04-16 22:02:12 +0700

    create proj.

---
+++ SoftStuGroup/SoftStuGroup/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SoftStuGroup.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
