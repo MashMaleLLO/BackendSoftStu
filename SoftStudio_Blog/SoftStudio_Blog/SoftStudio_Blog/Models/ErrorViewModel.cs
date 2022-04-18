c9ea3f2af00bf435ab6dadb6d275f8c0a2a449e2
Nontapan <coffee.nontapan@gmail.com>
2022-04-18 19:55:27 +0700

    Create New Project - SoftStudio_Blog

---
+++ SoftStudio_Blog/SoftStudio_Blog/SoftStudio_Blog/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SoftStudio_Blog.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
